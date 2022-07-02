1e82cacc36545e641278bfb2628d6e9782bf3d01
Jefferson Nah <hello@renovatio3d.com>
2022-07-02 11:30:29 -0700

    Initial commit

--- Calculator.py
+++ Calculator.py
@@ -27,3 +27,48 @@ elif Operation == 2:
     Number_2 = int(input("Please enter second number: "))
 
     print("Result: ", Number_1 - Number_2)
+
+elif Operation == 3:
+    Number_1 = int(input("Please enter first number: "))
+    Number_2 = int(input("Please enter second number: "))
+
+    print("Result: ", Number_1 * Number_2)
+
+elif Operation == 4:
+    Number_1 = int(input("Please enter first number: "))
+    Number_2 = int(input("Please enter second number: "))
+
+    print("Result: ", Number_1 / Number_2)
+
+elif Operation == 5:
+    Number_1 = int(input("Please enter a number: "))
+
+    print("Result: ", Number_1 ** 2)
+
+elif Operation == 6:
+    Weight = int(input("Please enter Weight: "))
+    Height = int(input("Please enter Height: "))
+
+    print("Result: ", Weight / (Height ** 2))
+
+elif Operation == 7:
+    Base = int(input("Please enter the base: "))
+    Height = int(input("Please enter the height: "))
+
+    print("Result: ", Base / (Height ** 2))
+
+elif Operation == 8:
+    Side = int(input("Please enter the side: "))
+
+    print("Result: ", Side ** 2)
+
+elif Operation == 9:
+    Number = int(input("Please enter a number: "))
+
+    print("Result: ", Number ** 0.5)
+
+elif Operation == 0:
+    print("Program is shutting down....")
+
+else:
+    print("Invalid operation..")
