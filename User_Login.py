e741c97b1635b199b7c6c9be81558e12ba98c11d
Jefferson Nah <hello@renovatio3d.com>
2022-07-02 13:08:23 -0700

    Completion basic User Login program

--- User_Login.py
+++ User_Login.py
@@ -1,17 +1,22 @@
-Sys_User_Name = "Jeffnah"
+Sys_User_Name = "JeffNah"
 Sys_Password = "1229"
 
 Attempts = 3
 while True:
     User_Name = input("Please enter user name: ")
     User_Password = input("Please enter your password: ")
-if User_Name != Sys_User_Name and User_Password == Sys_Password:
-    print("Wrong user name..")
-    Attempts = -1
-elif User_Name == Sys_User_Name and User_Password != Sys_Password:
-    print("Wrong password..")
-    Attempts =-1
-elif User_Name != Sys_User_Name and User_Password != Sys_Password:
-    print("Wrong user name and password..")
-    Attempts =-1
-
+    if User_Name != Sys_User_Name and User_Password == Sys_Password:
+        print("Wrong user name..")
+        Attempts = -1
+    elif User_Name == Sys_User_Name and User_Password != Sys_Password:
+        print("Wrong password..")
+        Attempts = -1
+    elif User_Name != Sys_User_Name and User_Password != Sys_Password:
+        print("Wrong user name and password..")
+        Attempts = -1
+    else:
+        print("Logged-in Successfully...")
+    break
+    if Attempts == 0:
+        print("You no longer have access. Please contact your system administrator")
+        break
