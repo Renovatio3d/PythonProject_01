1fdc05aa2855efa9fca702dd1bbae54217e605db
Jefferson Nah <hello@renovatio3d.com>
2022-07-02 12:46:28 -0700

    Configuration changes on Development User Login program

--- User_Login.py
+++ User_Login.py
@@ -5,6 +5,13 @@ Attempts = 3
 while True:
     User_Name = input("Please enter user name: ")
     User_Password = input("Please enter your password: ")
-
-
+if User_Name != Sys_User_Name and User_Password == Sys_Password:
+    print("Wrong user name..")
+    Attempts = -1
+elif User_Name == Sys_User_Name and User_Password != Sys_Password:
+    print("Wrong password..")
+    Attempts =-1
+elif User_Name != Sys_User_Name and User_Password != Sys_Password:
+    print("Wrong user name and password..")
+    Attempts =-1
 
