4002d80f9c333cdeaab22aed8c0c6d3b8194e103
Jefferson Nah <hello@renovatio3d.com>
2022-07-02 12:21:36 -0700

    Applied changes on Development User Login program

---
+++ User_Login.py
@@ -0,0 +1,10 @@
+Sys_User_Name = "Jeffnah"
+Sys_Password = "1229"
+
+Attempts = 3
+while True:
+    User_Name = input("Please enter user name: ")
+    User_Password = input("Please enter your password: ")
+
+
+
